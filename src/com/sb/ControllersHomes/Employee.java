5f643a4732ba9377a6dd57447dfde131bfb76354
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 <shitalbhosale0123@gmail.com>
2021-10-07 18:58:55 +0530

    Frist commit is here

---
+++ src/com/sb/ControllersHomes/Employee.java
@@ -0,0 +1,18 @@
+package com.sb.ControllersHomes;
+
+public class Employee 
+{
+	public int carGear()
+	{
+		System.out.println("6 gear car  is her  I 20");
+		
+		return 6;
+	}
+	
+	public static void main(String[] args) {
+		
+		Employee e=new Employee();
+		e.carGear();
+	}
+
+}
